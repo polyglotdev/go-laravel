4deb31786ea8ce330464e69a941063d2863b2a4c
Dom Hallan <dom@domhallan.com>
2024-05-08 05:16:46 -0400

    Refactor main function in Go app
    The main function of the Go application has been significantly refactored. The import statements and calls to external functions have been removed, simplifying the code structure. This change results in a cleaner, more readable main function.

--- myapp/main.go
+++ myapp/main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/polyglotdev/celeritas"
-)
-
 func main() {
-	result := celeritas.TestFunc(10, 10)
-	fmt.Println(result)
-
-	result = celeritas.TestFunc2(10, 20)
-	fmt.Println(result)
-
-	result = celeritas.TestFunc3(10, 30)
-	fmt.Println(result)
 }
