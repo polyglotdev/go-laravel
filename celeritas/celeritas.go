b9ab5d5d8fa524d441d1d722e6c7aceaeb9288ee
Dom Hallan <dom@domhallan.com>
2024-05-08 05:33:57 -0400

    Introduced Celeritas framework
    The commit introduces the Celeritas framework with its main struct and version constant. It includes methods for creating a new instance of the framework, initializing it with specified paths, and creating directories if they do not exist. Error handling has been added to these operations.

--- celeritas/celeritas.go
+++ celeritas/celeritas.go
@@ -1,1 +1,36 @@
 package celeritas
+
+const (
+	// Version is the current version of the Celeritas framework.
+	Version = "1.0.0"
+)
+
+// Celeritas is the main struct for the Celeritas framework.
+type Celeritas struct {
+	AppName string
+	Debug   bool
+	Version string
+}
+
+func (c Celeritas) New(rootPath string) error {
+	// create the root folder
+	err := c.CreateDirIfNotExist(rootPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Celeritas) Init(p initPaths) error {
+	//	get the root path
+	root := p.rootPath
+	// check if folders exist
+	for _, folderPath := range p.folderNames {
+		// create folderPath if it doesn't exist
+		err := c.CreateDirIfNotExist(root + "/" + folderPath)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
