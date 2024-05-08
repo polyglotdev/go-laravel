56ae39cae1b79664498364cb5c3cc290b4f65aa2
Dom Hallan <dom@domhallan.com>
2024-05-08 07:09:25 -0400

    Add .env file support and directory initialization
    - Implemented functionality to load environment variables from a .env file using the godotenv library.
    - Added a method to check for the existence of a .env file in the project root path, creating one with default values if it doesn't exist.
    - Enhanced the Init function to initialize directory structure for a Celeritas project. It now creates specified folders if they don't already exist.
    - Improved error handling and added comments for better code readability.

--- myapp/vendor/github.com/polyglotdev/celeritas/celeritas.go
+++ myapp/vendor/github.com/polyglotdev/celeritas/celeritas.go
@@ -1,5 +1,10 @@
 package celeritas
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
 const (
 	// Version is the current version of the Celeritas framework.
 	Version = "1.0.0"
@@ -33,19 +38,49 @@ func (c Celeritas) New(rootPath string) error {
 	if err != nil {
 		return err
 	}
+
+	err = c.checkDotEnv(rootPath)
+	if err != nil {
+		return err
+	}
+
+	// read .env file
+	err = godotenv.Load(rootPath + "/.env")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
+// Init initializes the directory structure for a Celeritas project.
+// It takes an initPaths struct as an argument which contains the root path and the names of the folders to be created.
+// It iterates over the folder names, and for each one, it calls the CreateDirIfNotExist method.
+// If the CreateDirIfNotExist method returns an error, it immediately returns this error.
+// If no errors occur during the folder creation, it returns nil.
 func (c Celeritas) Init(p initPaths) error {
-	//	get the root path
+	// Get the root path from the initPaths struct.
 	root := p.rootPath
-	// check if folders exist
+	// Iterate over the folder names in the initPaths struct.
 	for _, folderPath := range p.folderNames {
-		// create folderPath if it doesn't exist
+		// For each folder name, call the CreateDirIfNotExist method with the full path.
+		// If an error occurs, return it immediately.
 		err := c.CreateDirIfNotExist(root + "/" + folderPath)
 		if err != nil {
 			return err
 		}
 	}
+	// If no errors occurred during the folder creation, return nil.
+	return nil
+}
+
+// checkDotEnv checks if the .env file exists in the root path of the project.
+// If the file does not exist, it creates a new .env file with the default values.
+func (c *Celeritas) checkDotEnv(path string) error {
+	err := c.CreateFileIfNotExist(fmt.Sprintf("%s/.env", path))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- myapp/vendor/github.com/polyglotdev/celeritas/helpers.go
+++ myapp/vendor/github.com/polyglotdev/celeritas/helpers.go
@@ -14,13 +14,42 @@ import "os"
 // Returns:
 // If the directory exists or is successfully created, it returns nil.
 // If there is an error creating the directory, it returns the error.
-func (c Celeritas) CreateDirIfNotExist(path string) error {
+func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.MkdirAll(path, mode)
 		if err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+// CreateFileIfNotExist is a method on the Celeritas struct.
+// It checks if a file at the provided path exists.
+// If the file does not exist, it creates the file with the specified mode.
+// The mode is set to 0644, which means the owner can read and write,
+// while others can only read.
+//
+// Parameters:
+// path: A string representing the path of the file to check or create.
+//
+// Returns:
+// If the file exists or is successfully created, it returns nil.
+// If there is an error creating the file, it returns the error.
+func (c *Celeritas) CreateFileIfNotExist(path string) error {
+	const mode = 0644
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		file, err := os.Create(path)
+		if err != nil {
+			return err
+		}
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				return
+			}
+		}(file)
 	}
 	return nil
 }
